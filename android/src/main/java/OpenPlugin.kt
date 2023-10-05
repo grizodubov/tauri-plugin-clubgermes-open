d8be216a294f17383a88513840cf43a7ca3a85e3
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-10-05 01:32:05 -0400

    chore: lints

--- android/src/main/java/OpenPlugin.kt
+++ android/src/main/java/OpenPlugin.kt
@@ -2,47 +2,54 @@ package com.plugin.open
 
 import android.app.Activity
 import android.content.Intent
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
-import app.tauri.annotation.Command
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import app.tauri.annotation.TauriPlugin
-import app.tauri.plugin.JSObject
 import app.tauri.plugin.Plugin
-import app.tauri.plugin.Invoke
 
 @TauriPlugin
-class OpenPlugin(private val activity: Activity): Plugin(activity) {
-    override fun load(webView: WebView){
+class OpenPlugin(private val activity: Activity) : Plugin(activity) {
+    override fun load(webView: WebView) {
         super.load(webView)
 
         val client = webView.webViewClient
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                val url = request.url.toString()
-                val isHttp = url.startsWith("http://") || url.startsWith("https://")
+        webView.webViewClient =
+                object : WebViewClient() {
+                    override fun shouldOverrideUrlLoading(
+                            view: WebView,
+                            request: WebResourceRequest
+                    ): Boolean {
+                        val url = request.url.toString()
+                        val isHttp = url.startsWith("http://") || url.startsWith("https://")
 
-                val isTauri = request.url.host == "tauri.localhost"
+                        val isTauri = request.url.host == "tauri.localhost"
 
-                if (isHttp && !isTauri) {
-                    val intent = Intent(Intent.ACTION_VIEW, request.url)
-                    activity.startActivity(intent)
+                        if (isHttp && !isTauri) {
+                            activity.startActivity(Intent(Intent.ACTION_VIEW, request.url))
 
-                    return true
+                            return true
+                        }
+
+                        return client.shouldOverrideUrlLoading(view, request)
+                    }
+
+                    override fun shouldInterceptRequest(
+                            view: WebView,
+                            request: WebResourceRequest
+                    ): WebResourceResponse? {
+                        return client.shouldInterceptRequest(view, request)
+                    }
+
+                    @android.annotation.SuppressLint("WebViewClientOnReceivedSslError")
+                    override fun onReceivedSslError(
+                            view: WebView?,
+                            handler: android.webkit.SslErrorHandler?,
+                            error: android.net.http.SslError?
+                    ) {
+                        client.onReceivedSslError(view, handler, error)
+                    }
                 }
-
-                return client.shouldOverrideUrlLoading(view, request)
-            }
-
-            override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
-                return client.shouldInterceptRequest(view, request)
-            }
-
-            @android.annotation.SuppressLint("WebViewClientOnReceivedSslError")
-            override fun onReceivedSslError(view: WebView?, handler: android.webkit.SslErrorHandler?, error: android.net.http.SslError?) {
-                client.onReceivedSslError(view, handler, error)
-            }
-        }
     }
 }
--- src/mobile.rs
+++ src/mobile.rs
@@ -1,8 +1,5 @@
 use serde::de::DeserializeOwned;
-use tauri::{
-    plugin::{PluginApi, PluginHandle},
-    AppHandle, Runtime,
-};
+use tauri::{plugin::PluginApi, Runtime};
 
 #[cfg(target_os = "android")]
 const PLUGIN_IDENTIFIER: &str = "com.plugin.open";
@@ -13,10 +10,10 @@ tauri::ios_plugin_binding!(init_plugin_open);
 // initializes the Kotlin or Swift plugin classes
 pub fn init<R: Runtime, C: DeserializeOwned>(api: PluginApi<R, C>) -> crate::Result<()> {
     #[cfg(target_os = "android")]
-    let handle = api.register_android_plugin(PLUGIN_IDENTIFIER, "OpenPlugin")?;
+    let _ = api.register_android_plugin(PLUGIN_IDENTIFIER, "OpenPlugin")?;
 
     #[cfg(target_os = "ios")]
-    let handle = api.register_ios_plugin(init_plugin_open)?;
+    let _ = api.register_ios_plugin(init_plugin_open)?;
 
     Ok(())
 }
