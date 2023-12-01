321aa44894f6ad9e1732cec5be6657488309a905
Valerii <55758039+grizodubov@users.noreply.github.com>
2023-12-01 14:11:37 +0300

    added some gradle files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:8.1.4")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+  }
+}
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -34,7 +45,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
